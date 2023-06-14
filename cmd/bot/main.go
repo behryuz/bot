48f39be41fc72f68cd07cf3c4b7aadc32e07979d
behryus <behruz.yousupov@gmail.com>
2023-06-14 17:42:56 +0500

    add help and list commands

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/behryuz/bot/internal/service/product"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
@@ -26,14 +27,39 @@ func main() {
 
 	updates := bot.GetUpdatesChan(u)
 
+	productService := product.NewService()
+
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
-			//msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
+		switch update.Message.Command() {
+		case "help":
+			helpCommand(bot, update.Message)
+		case "list":
+			listCommand(bot, update.Message, productService)
+		default:
+			defaultBehaviour(bot, update.Message)
 		}
 	}
+}
+
+func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
+		"/help - help\n"+
+			"/list - list of products")
+	bot.Send(msg)
+}
+
+func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService *product.Service) {
+	outputMsq := ""
+	products := productService.List()
+	for _, p := range products {
+		outputMsq += p.Title + "\n"
+	}
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMsq)
+	bot.Send(msg)
+}
+
+func defaultBehaviour(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
+	log.Printf("[%s] %s", inputMessage.From.UserName, inputMessage.Text)
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "You wrote: "+inputMessage.Text)
+	bot.Send(msg)
 }
